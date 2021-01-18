21042ed50316d4e50cf30977e536e7a1e56b7c35
TudorBucur19 <tudor.bucur88@gmail.com>
2021-01-18 02:00:26 +0200

    first commit

---
+++ src/contexts/BookContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useEffect, useState } from 'react';
+import uuid from 'react-uuid';
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+  
+  const [books, setBooks] = useState(
+    () => {
+      const localData = localStorage.getItem('books');
+      return localData ? JSON.parse(localData) : []
+    }
+  );
+
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books))
+  }, [books]);
+
+  const addBook = (title, author) => {
+    setBooks([...books, {title, author, id: uuid()}]);
+  };
+  const removeBook = (id) => {
+    setBooks(books.filter(book => book.id !== id));
+  }
+
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+}
+ 
+export default BookContextProvider;
